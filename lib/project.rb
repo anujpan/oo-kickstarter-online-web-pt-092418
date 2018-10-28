3f0313de9fe9bb0ae9c1b0d3eb13db21c5ad7d37
Anuj Panchal <anujpan1@gmail.com>
2018-10-28 23:16:29 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,15 +1,37 @@
 require 'pry'
 
-class Project
-  attr_accessor :title, :backers
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Project
+#   attr_accessor :title, :backers
   
-  def initialize(title)
-    @title = title
-    @backers = []
-  end
+#   def initialize(title)
+#     @title = title
+#     @backers = []
+#   end
   
-  def add_backer(backer)
-      @backers.push(backer)
-      backer.backed_projects.push(self)
-  end
-end
+#   def add_backer(backer)
+#       @backers.push(backer)
+#       backer.backed_projects.push(self)
+#   end
+# end
