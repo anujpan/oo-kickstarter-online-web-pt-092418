b638848df046f50180ca01d89d4354f27ff1559e
Anuj Panchal <anujpan1@gmail.com>
2018-10-28 23:16:19 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,15 +1,40 @@
 require 'pry'
 
-class Backer
-  attr_accessor :name, :backed_projects
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Backer
+#   attr_accessor :name, :backed_projects
   
-  def initialize(name)
-    @name = name
-    @backed_projects = []
-  end
+#   def initialize(name)
+#     @name = name
+#     @backed_projects = []
+#   end
   
-  def back_project(project)
-    @backed_projects.push(project)
-    project.backers.push(self)
-  end
-end
+#   def back_project(project)
+#     @backed_projects.push(project)
+#     project.backers.push(self)
+#   end
+# end
